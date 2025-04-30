7425b87977b76736a44dcc979acb34d47be75b02
Pablo Villar Morón <pablo.villar.moron@gmail.com>
2025-04-30 16:19:31 +0200

    lab-done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 const buttonAddRandom = document.querySelector("#btn-add-random");
 const tableBody = document.querySelector("tbody#contacts");
 
-
 // ITERATION 0 | Example Row
 // Splice 1 element from the contacts array at the random index
 const randomIndex = Math.floor(Math.random() * contacts.length);
@@ -30,31 +29,55 @@ exampleRow.innerHTML = `
 
 tableBody.appendChild(exampleRow);
 
-
-
-
-
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
+threeContacts.forEach((contact) => {
+  const exampleRow = document.createElement("tr");
+  exampleRow.innerHTML = ` <td>
+    <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>`;
+  const btnLike = exampleRow.querySelector(".btn-like");
+  btnLike.addEventListener("click", () => {
+    btnLike.classList.toggle("selected");
+  });
+  const btnDelete = exampleRow.querySelector(".btn-delete");
+  btnDelete.addEventListener("click", () => {
+    exampleRow.remove();
+  });
+  tableBody.appendChild(exampleRow);
+});
 
+// ITERATION 2 - Delete Buttons
 
-  
-  // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
+// Your code goes here ...
+//threeContacts.forEach((contact) => {
+const btnDelete = exampleRow.querySelector(".btn-delete");
+btnDelete.addEventListener("click", () => {
+  exampleRow.remove();
+});
+//tableBody.appendChild(exampleRow);
+//})
 
-  // ITERATION 3 - Like Buttons
+// ITERATION 3 - Like Buttons
 
-  // Your code goes here ...
-
-  
-  
-
+// Your code goes here ...
+const btnLike = exampleRow.querySelector(".btn-like");
+btnLike.addEventListener("click", () => {
+  btnLike.classList.toggle("selected");
+});
 
 // Bonus: ITERATION 4 - Add Random Contacts
 
